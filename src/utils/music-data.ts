7773c31cf8e7fb7421cee85a42edf71e7818dd0a
Juhamikael <contact.juhamusic@gmail.com>
2023-04-19 17:30:41 +0300

    Update music-data.ts

--- src/utils/music-data.ts
+++ src/utils/music-data.ts
@@ -1,4 +1,13 @@
 const musicData = {
+  revived: {
+    name: "Revived.",
+    data: {
+      youtube: ["ZdMKFdqNCQA", "n3rcLNOQLFU", "37V3uaeylY8", "bdds_kHKRLk"],
+      spotify:
+        "https://open.spotify.com/embed/playlist/2mBxbEOirYYMZyNEY5oUzv?utm_source=generator",
+      soundcloud: "revived?si=994383753d95456084586722f51ad1e7",
+    },
+  },
   "nino-rivera": {
     name: "Nino Rivera",
     data: {
@@ -15,8 +24,33 @@ const musicData = {
         "vJD4NU30PAs",
         "OBQ_c-wgr24",
       ],
-      spotify: "https://open.spotify.com/artist/0ECUFMagjydidZSodMlm8H",
+
+      spotify:
+        "https://open.spotify.com/embed/artist/0ECUFMagjydidZSodMlm8H?utm_source=generator",
       soundcloud: "nino-rivera?si=d692eb3390174e41b07e2b61b64fa792",
+    },
+  },
+  jolex: {
+    name: "Jol3x",
+    data: {
+      youtube: [
+        "1fB-w0_Esks",
+        "ZdMKFdqNCQA",
+        "n3rcLNOQLFU",
+        "37V3uaeylY8",
+        "rLfUSPOhSpo",
+        "D30NeDxTfts",
+        "bdds_kHKRLk",
+        "k7lcpHfrZ_Q",
+        "MGrC6e407rQ",
+        "Bfd49UZhlPc",
+        "9VFiVFgtmuQ",
+        "zQi7Lv5KzKI",
+        "fRbWLnPy1tU",
+      ],
+      spotify:
+        "https://open.spotify.com/embed/artist/4BR8Vv00g1u2KyAY0DzXUE?utm_source=generator",
+      soundcloud: "jol3x?si=4de7a2d725ed4b48a0304fd455247636",
     },
   },
   "juha-mikael": {
@@ -41,31 +75,8 @@ const musicData = {
         "1UoTsu-y9xY",
       ],
       spotify:
-        "https://open.spotify.com/artist/2heJl7jzvTYoHyR5xwSSea?si=gLjTaLmzQhSQ_C0pUTI0Tw",
-      soundcloud: "juha-mikael?si=1e304815349b4cfa9b17cf5a5e4ce026",
-    },
-  },
-  jolex: {
-    name: "Jol3x",
-    data: {
-      youtube: [
-        "1fB-w0_Esks",
-        "ZdMKFdqNCQA",
-        "n3rcLNOQLFU",
-        "37V3uaeylY8",
-        "rLfUSPOhSpo",
-        "D30NeDxTfts",
-        "bdds_kHKRLk",
-        "k7lcpHfrZ_Q",
-        "MGrC6e407rQ",
-        "Bfd49UZhlPc",
-        "9VFiVFgtmuQ",
-        "zQi7Lv5KzKI",
-        "fRbWLnPy1tU",
-      ],
-      spotify:
-        "https://open.spotify.com/artist/4BR8Vv00g1u2KyAY0DzXUE?si=661556705f8b402a",
-      soundcloud: "jol3x?si=4de7a2d725ed4b48a0304fd455247636",
+        "https://open.spotify.com/embed/artist/2heJl7jzvTYoHyR5xwSSea?utm_source=generator",
+      soundcloud: "juha-mikael?si=aa5524b03b9c48b08988470b37fccf84",
     },
   },
 };
